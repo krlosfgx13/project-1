75aa0672780cb50d87c5f4416da129f6dfc3a16f
krlosfgx13 <gonzalezcarlosc9@gmail.com>
2025-03-14 16:32:19 -0600

    consume REST API example.

--- src/app/pages/rest-api-categories/rest-api-categories.component.ts
+++ src/app/pages/rest-api-categories/rest-api-categories.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CategoryResponse } from '../../interfaces/CategoryResponse';
+import { ApiRestService } from '../../services/api-rest.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-rest-api-categories',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './rest-api-categories.component.html',
   styleUrl: './rest-api-categories.component.css'
 })
-export class RestApiCategoriesComponent {
+export class RestApiCategoriesComponent implements OnInit {
+
+  data: Array<CategoryResponse>;
+
+  constructor(private service: ApiRestService){ }
+  ngOnInit(): void {
+    this.performGetRequest();
+  }
+
+  performGetRequest(){
+    this.service.getCategories().subscribe({
+      next: data => {
+        console.log(data);
+        this.data = data
+      }, 
+      error: error => {
+        console.log(error);
+      }
+    }
+    );
+  }
 
 }
--- src/app/services/api-rest.service.ts
+++ src/app/services/api-rest.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from './../../environments/environment';
 import { Observable } from 'rxjs'; //manages the responses.
 import { HttpClient } from '@angular/common/http'; //manages the requests.
+import { CategoryRequest } from '../interfaces/CategoryRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,7 @@ export class ApiRestService {
 
   api: string;
   header = {
-    'content-type': 'application/json',
-    'Authorization': 'Bearer '
+    'content-type': 'application/json'
   }
 
   constructor(private http: HttpClient) { 
@@ -22,5 +22,7 @@ export class ApiRestService {
     return this.http.get(`${this.api}categorias`, {headers: this.header});
   }
 
-  
+  addCategory(jsonRequest: CategoryRequest): Observable<any> {
+    return this.http.post(`${this.api}categorias`,jsonRequest, {headers: this.header});
+  }  
 }
