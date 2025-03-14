86b3f6be6fdaa06053095b01d9c13bd1981075e3
krlosfgx13 <gonzalezcarlosc9@gmail.com>
2025-03-13 18:24:39 -0600

    new expansion component

---
+++ src/app/pages/material-expansion/material-expansion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-material-expansion',
+  imports: [],
+  templateUrl: './material-expansion.component.html',
+  styleUrl: './material-expansion.component.css'
+})
+export class MaterialExpansionComponent {
+
+}
